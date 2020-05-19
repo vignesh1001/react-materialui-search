7efe47da85dcec7622475707f3c5e01d0418cf1d
vignesh1001 <vignesh1001@gmail.com>
2020-05-19 19:32:20 +0530

    1 file modified

--- ListPage.js
+++ ListPage.js
@@ -12,27 +12,36 @@ import Select from "@material-ui/core/Select";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { DesktopMacIcon } from '@material-ui/icons';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { withStyles } from "@material-ui/core/styles";
+import {
+  DesktopMac,
+  LaptopMac,
+  PhoneAndroid,
+  PhoneIphone,
+  CloudDownload,
+  Save as SaveIcon,
+} from "@material-ui/icons";
 
-function createData(deviceName,description) {
-  return { deviceName,description };
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+function createData(deviceName, description) {
+  return { deviceName, description };
 }
 
 const rows = [
-  createData('Device 1','Test 1222423442342'),
-  createData('Mobile 1','Test 55656565'),
-  createData('iPhone 1','Test 957545'),
-  createData('Android 1','Test 8888888'),
-  createData('Device 1','Test 1111111111'),
+  createData("Laptop 1", "Test 1222423442342"),
+  createData("Mobile 1", "Test 55656565"),
+  createData("iPhone 1", "Test 957545"),
+  createData("Android 1", "Test 8888888"),
+  createData("Desktop 1", "Test 1111111111")
 ];
 
-const useStyles = makeStyles(theme => ({
+const useStyles = (theme => ({
   root: {},
   formControl: {
     width: "100%"
@@ -44,129 +53,159 @@ const useStyles = makeStyles(theme => ({
   filterHeader: {
     ...this.clearPadding,
     textAlign: "center"
+  },
+  deviceName: {
+    paddingLeft: 10,
+    paddingRight: 0,
+    whiteSpace: 'nowrap',
+  },
+  deviceIcon: {
+    paddingLeft: 0,
+    paddingRight: 0
   }
 }));
-
-function ListPage() {
-  const classes = useStyles();
-  const [state, setState] = useState({
-    ipaddress: "",
-    hostname: "",
-    qidcve: "",
-    dropdown: ""
-  });
-  const handleChange = (event, value) => {
+class ListPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ipaddress: "",
+      hostname: "",
+      qidcve: "",
+      dropdown: ""
+    };
+  }
+  handleChange = (event, value) => {
     const { name, value } = event.target;
-    setState(prevState => ({ ...prevState, [name]: value }));
+    this.setState(prevState => ({ ...prevState, [name]: value }));
   };
-  return (
-    <div className={classes.root}>
-      <Container fixed maxWidth="sm">
-        <Grid container spacing={1}>
-          <Grid item xs={6} style={{ height: "100vh" }}>
-            <form className={classes.root} noValidate>
-              <ListItem divider className={classes.filterHeader}>
-                <ListItemText primary="General" />
-              </ListItem>
-              <Divider />
-              <List component="div" aria-label="mailbox folders">
-                <ListItem divider className={classes.clearPadding}>
-                  <TextField
-                    name="ipaddress"
-                    label="IP Address"
-                    variant="outlined"
-                    color="secondary"
-                    value={state.ipaddress}
-                    onChange={handleChange}
-                  />
+
+  getDeviceIcon = deviceName => {
+    if (deviceName.indexOf("Desktop ") > -1) {
+      return <DesktopMac />;
+    } else if (deviceName.indexOf("Laptop ") > -1) {
+      return <LaptopMac />;
+    } else if (deviceName.indexOf("Android ") > -1 || deviceName.indexOf("Mobile ") > -1) {
+      return <PhoneAndroid />;
+    } else if (deviceName.indexOf("iPhone ") > -1) {
+      return <PhoneIphone />;
+    }
+  };
+  render() {
+    const { classes } = this.props;
+    const state = this.state;
+    return (
+      <div className={classes.root}>
+        <Container fixed maxWidth="sm">
+          <Grid container spacing={1}>
+            <Grid item xs={3} style={{ height: "100vh" }}>
+              <form className={classes.root} noValidate>
+                <ListItem divider className={classes.filterHeader}>
+                  <ListItemText primary="General" />
                 </ListItem>
-                <ListItem divider className={classes.clearPadding}>
-                  <TextField
-                    name="hostname"
-                    label="Host Name"
-                    variant="outlined"
-                    value={state.hostname}
-                    onChange={handleChange}
-                    color="secondary"
-                  />
-                </ListItem>
-                <ListItem divider className={classes.clearPadding}>
-                  <TextField
-                    name="qidcve"
-                    label="QID / CVE"
-                    variant="outlined"
-                    value={state.qidcve}
-                    color="secondary"
-                    onChange={handleChange}
-                  />
-                </ListItem>
-                <ListItem divider className={classes.clearPadding}>
-                  <FormControl
-                    variant="outlined"
-                    className={classes.formControl}
-                  >
-                    <InputLabel id="demo-simple-select-outlined-label">
-                      Select
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-outlined-label"
-                      name="dropdown"
-                      value={10}
+                <Divider />
+                <List component="div" aria-label="mailbox folders">
+                  <ListItem divider className={classes.clearPadding}>
+                    <TextField
+                      name="ipaddress"
+                      label="IP Address"
+                      variant="outlined"
                       color="secondary"
-                      value={state.dropdown}
-                      onChange={handleChange}
-                      label="dropdown"
+                      value={state.ipaddress}
+                      onChange={this.handleChange}
+                    />
+                  </ListItem>
+                  <ListItem divider className={classes.clearPadding}>
+                    <TextField
+                      name="hostname"
+                      label="Host Name"
+                      variant="outlined"
+                      value={state.hostname}
+                      onChange={this.handleChange}
+                      color="secondary"
+                    />
+                  </ListItem>
+                  <ListItem divider className={classes.clearPadding}>
+                    <TextField
+                      name="qidcve"
+                      label="QID / CVE"
+                      variant="outlined"
+                      value={state.qidcve}
+                      color="secondary"
+                      onChange={this.handleChange}
+                    />
+                  </ListItem>
+                  <ListItem divider className={classes.clearPadding}>
+                    <FormControl
+                      variant="outlined"
+                      className={classes.formControl}
                     >
-                      <MenuItem value="">
-                        <em>None</em>
-                      </MenuItem>
-                      <MenuItem value={10}>Ten</MenuItem>
-                      <MenuItem value={20}>Twenty</MenuItem>
-                      <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                  </FormControl>
+                      <InputLabel id="demo-simple-select-outlined-label">
+                        Select
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-outlined-label"
+                        name="dropdown"
+                        value={10}
+                        color="secondary"
+                        value={state.dropdown}
+                        onChange={this.handleChange}
+                        label="dropdown"
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </ListItem>
+                </List>
+                <ListItem divider className={classes.filterHeader}>
+                  <ListItemText primary="Software" />
                 </ListItem>
-              </List>
-              <ListItem divider className={classes.filterHeader}>
-                <ListItemText primary="Software" />
-              </ListItem>
-              <ListItem divider className={classes.filterHeader}>
-                <ListItemText primary="Hardware" />
-              </ListItem>
-            </form>
+                <ListItem divider className={classes.filterHeader}>
+                  <ListItemText primary="Hardware" />
+                </ListItem>
+              </form>
+            </Grid>
+            <Grid item xs={9} style={{ height: "100vh" }}>
+              <TableContainer component={Paper}>
+                <Table
+                  className={classes.table}
+                  size="small"
+                  aria-label="a dense table"
+                >
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align="right" />
+                      <TableCell align="right" />
+                      <TableCell align="right" />
+                      <TableCell align="center" className={classes.deviceName}>
+                        <SaveIcon /> <CloudDownload />
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map(row => (
+                      <TableRow key={row.name}>
+                        <TableCell align="left" className={classes.deviceName}>{row.deviceName}</TableCell>
+                        <TableCell align="center" className={classes.deviceIcon}>
+                          {this.getDeviceIcon(row.deviceName)}
+                        </TableCell>
+                        <TableCell align="right">{row.description}</TableCell>
+                        <TableCell align="right" />
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
           </Grid>
-          <Grid item xs={6} style={{ height: "100vh" }}>
-            <TableContainer component={Paper}>
-      <Table className={classes.table} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.deviceName}</TableCell>
-              <TableCell align="right"></TableCell>
-              <TableCell align="right">{row.description}</TableCell>
-              <TableCell align="right"></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default ListPage;
+export default withStyles(useStyles, { withTheme: true })(ListPage);
